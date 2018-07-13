af8cf61fd551aecd61a8dc27227f473f86f11dbf
AndreasMN <andycutter@godmail.dk>
2018-07-13 09:47:03 +0200

    drop db, delete cartify db, made new cartify:install:migration, made new db:create, db:migrate

--- db/migrate/20180713074464_create_cartify_order_items.cartify.rb
+++ db/migrate/20180713074464_create_cartify_order_items.cartify.rb
@@ -5,7 +5,7 @@ class CreateCartifyOrderItems < ActiveRecord::Migration[5.2]
       t.integer :quantity
       t.decimal :unit_price, precision: 8, scale: 2
       t.decimal :total_price, precision: 8, scale: 2
-      t.integer :dish_id, foreign_key: true
+      t.integer :product_id, foreign_key: true
       t.references :order,
                     foreign_key: { to_table: :cartify_orders }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_12_140246) do
+ActiveRecord::Schema.define(version: 2018_07_13_074465) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2018_07_12_140246) do
     t.integer "quantity"
     t.decimal "unit_price", precision: 8, scale: 2
     t.decimal "total_price", precision: 8, scale: 2
-    t.integer "dish_id"
+    t.integer "product_id"
     t.bigint "order_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
